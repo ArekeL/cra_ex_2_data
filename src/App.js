4cb02933241566481d4f651d73b39ca6bf9d5568
Arek Lechnik <alechnik@gmail.com>
2022-12-05 07:03:39 +0100

    show message

--- src/App.js
+++ src/App.js
@@ -2,11 +2,32 @@ import React, { Component } from "react";
 import "./App.css";
 
 // pseudo rosnąca baza danych
+const data = [
+	{ id: 1, title: "Wyświetl wiadomość 1", body: "Treść wiadomości numer 1" },
+	{ id: 2, title: "Wyświetl wiadomość 2", body: "Treść wiadomości numer 2" },
+];
+
+setInterval(() => {
+	const index = data.length + 1;
+	data.push({
+		id: index,
+		title: `Wyświetl wiadomość ${index}`,
+		body: `Treść wiadomości numer ${index}`,
+	});
+}, 2000);
 
 class App extends Component {
-	state = {};
+	state = {
+		comments: [...data],
+	};
 	render() {
-		return <div>Działa OK</div>;
+		const comments = this.state.comments.map((comments) => (
+			<div key={comments.id}>
+				<h4>{comments.title}</h4>
+				<p>{comments.body}</p>
+			</div>
+		));
+		return <div>{comments.reverse()}</div>;
 	}
 }
 
